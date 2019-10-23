f7053f696c96767eccb3de9f6fe04e31890f07fb
Mammadli Chingiz <tferzeliyev@std.qu.edu.az>
2019-10-23 11:52:52 +0400

    Initial commit

--- js/custom.js
+++ js/custom.js
@@ -182,3 +182,14 @@ jQuery(function($) {
   
   
   });
+
+// My codes
+var stickyOffset = $('#header').offset().top;
+
+$(window).scroll(function(){
+    var sticky = $('.header'),
+        scroll = $(window).scrollTop();
+
+    if (scroll > stickyOffset) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+});
